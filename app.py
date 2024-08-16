e44a3d3be42403d764496708637ea2c431a2461e
H252003 <mohamedhesham252003@gmail.com>
2024-08-16 18:22:31 +0300

    testing local for nouran

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
-from flask import Flask, Response, jsonify, request
+from flask import Flask, Response, jsonify, send_from_directory, request
 import cv2
 import numpy as np
-import requests
 
 # create the app
 app = Flask(__name__)
 
 
-# Load the Haarcascade from Google Drive
-def load_haarcascade(drive_url):
-    # Convert Google Drive URL to direct download link
-    file_id = drive_url.split('/')[-2]
-    direct_url = f'https://drive.google.com/uc?export=download&id={file_id}'
+def crop_face_and_return(image):
+    cropped_face = None
+    #create instance of haarcascade
+    detector = cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
+    #capture the face using the model
+    faces = detector.detectMultiScale(image, 1.1, 7)
+    #crop the face exactly
+    for (x, y, w, h) in faces:
+        cropped_face = image[y:y + h, x:x + w]
+    return cropped_face
 
-    response = requests.get(direct_url)
-    with open('haarcascade_frontalface_default.xml', 'wb') as file:
-        file.write(response.content)
-    return 'haarcascade_frontalface_default.xml'
 
-
-# Load the Haarcascade file
-cascade_path = load_haarcascade('https://drive.google.com/file/d/1iAxPzT8ssOM9ucUMT78KZBlJbA0MVfZX/view?usp=sharing')
-
-# Main application
+#main application
 @app.route('/')
 def index():
     return jsonify({"message": "Welcome to the face detection app"})
@@ -42,7 +38,7 @@ def generate_frames():
             return jsonify({"error": "Failed to decode image"}), 400
 
         # Create instance of face detection
-        detector = cv2.CascadeClassifier(cascade_path)
+        detector = cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
         # Convert the image to grayscale
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         # Detect faces
@@ -51,7 +47,14 @@ def generate_frames():
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
         # Return the image with rectangles around faces
-        return Response(cv2.imencode('.jpg', image)[1].tobytes(), mimetype='image/jpeg')
+        # return Response(cv2.imencode('.jpg', image)[1].tobytes(), mimetype='image/jpeg')
+        # return the coordinates of the faces
+        return jsonify({"faces": faces.tolist()})
+
+# Ensure that static files are served
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
 
 
 if __name__ == "__main__":
